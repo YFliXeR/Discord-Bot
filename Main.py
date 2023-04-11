c19eed0ef02293441866b35172aacafff3859274
YFliXeR <117351277+YFliXeR@users.noreply.github.com>
2023-04-11 14:21:51 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import asyncio
 from discord.ext import tasks
 from discord import Intents
 from dotenv import load_dotenv
+from pytz import timezone
 
 load_dotenv(dotenv_path="encrypt.env")
 
@@ -47,10 +48,13 @@ async def on_message(message):
     # Calculate date 7 days after input date and time
     new_datetime_obj = datetime_obj + datetime.timedelta(days=7)
     new_datetime_str = new_datetime_obj.strftime("%d %b, %Y %I:%M%p")
+    print("new_datetime_obj", new_datetime_obj)
+    print("new_datetime_str: " + new_datetime_str)
 
     # Save reminder date, time, account name, and channel ID to dictionary
     if account_name not in saved_reminders:
-      saved_reminders[account_name] = [(new_datetime_obj, message.channel.id)]
+      saved_reminders[account_name] = [(new_datetime_obj, account_name,
+                                        message.author.id)]
     else:
       saved_reminders[account_name].append(
         (new_datetime_obj, message.channel.id, message.author.id))
@@ -59,7 +63,9 @@ async def on_message(message):
       print(account_name, dates)
 
     # Create embed with confirmation message
-    embed = discord.Embed(title="Reminder added", description=f"Account: {account_name}", color=0x00ff00)
+    embed = discord.Embed(title="Reminder added",
+                          description=f"Account: {account_name}",
+                          color=0x00ff00)
     embed.add_field(name="Date and Time", value=new_datetime_str)
     # Send confirmation as embed
     await message.channel.send(embed=embed)
@@ -69,26 +75,52 @@ async def on_message(message):
 
 @tasks.loop(minutes=1)
 async def check_saved_dates():
-  now = datetime.datetime.now()
+  now = datetime.datetime.now(timezone('Egypt'))
   for account_name, dates in saved_reminders.items():
     for date in dates:
+
       date_obj, channel_id, author_id = date
+      # channel = client.get_channel(channel_id)
+
+      date_obj = timezone('Egypt').localize(date_obj)
+
+      # print(date_obj , now)
       if now >= date_obj:
-        # Get the channel where the message was sent
-        channel = client.get_channel(channel_id)
+        try:
 
-        # Get the member who added the reminder
-        member = await channel.guild.fetch_member(author_id)
+          member = await client.fetch_user(author_id)
+          # Create the embed with the reminder information
+          embed = discord.Embed(title="Reminder",
+                                description=f"Account: {account_name}",
+                                color=0x00ff00)
+          embed.add_field(name="Date and Time",
+                          value=date_obj.strftime('%d %b, %Y %I:%M%p'))
 
-        if member is not None:
-          # Create embed with reminder information
-          embed = discord.Embed(title="Reminder", description=f"Account: {account_name}", color=0x00ff00)
-          embed.add_field(name="Date and Time", value=date_obj.strftime('%d %b, %Y %I:%M%p'))
-          # Send reminder as embed
-          await channel.send(content=member.mention, embed=embed)
+          # Send the reminder as an embed in a DM to the user
+          await member.send(embed=embed)
+          saved_reminders[account_name].remove(date)
+        except:
+          print("")
+      #   # Get the channel where the message was sent
+      #   channel = client.get_channel(channel_id)
+      #   print("channel" + channel)
 
-        # Remove saved date from dictionary
-        saved_reminders[account_name].remove(date)
+      #   # Get the member who added the reminder
+      #   member = await channel.guild.fetch_member(author_id)
+      #   print("member: ", member)
+
+      #   if member is not None:
+      #     # Create embed with reminder information
+      #     embed = discord.Embed(title="Reminder",
+      #                           description=f"Account: {account_name}",
+      #                           color=0x00ff00)
+      #     embed.add_field(name="Date and Time",
+      #                     value=date_obj.strftime('%d %b, %Y %I:%M%p'))
+      #     # Send reminder as embed
+      #     await channel.send(content=member.mention, embed=embed)
+      #     print("embed" + embed)
+      #   # Remove saved date from dictionary
+      # saved_reminders[account_name].remove(date)
 
 
 client.run(os.getenv("TOKEN"))
